4395732c252912b89150de91f846f82769f7c9e0
My Name <maheshwagh113@gamil.com>
2026-05-30 13:26:53 +0530

    Sync backend server.js fixes with root repository

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,9 @@ const taskRoutes = require('./routes/taskRoutes');
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
+app.get('/', (req, res) => res.send('API is running...'));
+app.get('/favicon.ico', (req, res) => res.status(204).end());
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/taskmanager')
   .then(() => console.log('MongoDB connected'))
